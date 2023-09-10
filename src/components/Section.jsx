f433ad5e620194eefe351b34914c6c7acc12b45c
bacho1927 <bacho.karashvili@gmail.com>
2023-09-10 16:27:42 +0400

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,17 +2,17 @@ import { firstSection } from "./Data"
 
 function Section() {
 
-    const sectionData = firstSection.map(section => {
+    const sectionData = firstSection.map((section, index) => {
         return (
-            <div key={section.id} className="py-20  bg-black border-b-[10px] border-gray-700 min-w-[500px]">
-                <div className="flex flex-col items-center  lg:flex lg:flex-row lg:max-w-[1100px] bg-red mx-auto" >
+            <div key={index} className="py-20  bg-black border-b-[10px] border-gray-700 min-w-[500px]">
+                <div className={`flex flex-col items-center  ${index % 2 === 0 ? 'lg:flex lg:flex-row' : 'lg:flex lg:flex-row-reverse'} lg:max-w-[1100px] bg-red mx-auto`}>
                     <div className="flex flex-col justify-center " >
                         <h1 className="text-5xl font-bold text-white text-center lg:text-left">{section.sectionHeader}</h1>
                         <p className="text-white text-2xl  my-4 text-center lg:text-left">{section.sectionParagraph}</p>
                     </div>
-                    <div className="relative z-[1] ">
-                        <img src={section.sectionPhoto} className="w-[90%] mx-auto  lg:w-full " />
-                        <div className={` absolute    z-[-1] ${section.id === 3 ? 'top-[30px] w-[295px] left-[90px]' : 'top-[70px] right-[65px] w-[72%]'
+                    <div className="relative z-[1] w-[80%] ">
+                        <img src={section.sectionPhoto} className="mx-auto  lg:w-full " />
+                        <div className={` absolute  z-[-1] ${section.id === 3 ? 'top-[40px] w-[350px] left-[105px]' : 'top-[80px] right-[75px] w-[73%]'
                             } `}>
                             <video autoPlay muted loop >
                                 <source src={section.sectionVideo} type='video/mp4' />
