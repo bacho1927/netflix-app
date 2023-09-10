e59acef71a53d9f02bae5c7e31990d298bc724e1
bacho1927 <bacho.karashvili@gmail.com>
2023-09-10 15:42:27 +0400

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,13 +5,13 @@ function Section() {
     const sectionData = firstSection.map(section => {
         return (
             <div key={section.id} className="py-20  bg-black border-b-[10px] border-gray-700 min-w-[500px]">
-                <div className="flex flex-col items-center px-2 lg:flex lg:flex-row lg:px-32 bg-red" >
-                    <div className="flex flex-col justify-center px-10" >
+                <div className="flex flex-col items-center  lg:flex lg:flex-row lg:max-w-[1100px] bg-red mx-auto" >
+                    <div className="flex flex-col justify-center " >
                         <h1 className="text-5xl font-bold text-white text-center lg:text-left">{section.sectionHeader}</h1>
-                        <p className="text-white text-xl  my-4 text-center lg:text-left">{section.sectionParagraph}</p>
+                        <p className="text-white text-2xl  my-4 text-center lg:text-left">{section.sectionParagraph}</p>
                     </div>
-                    <div className="relative z-[1]">
-                        <img src={section.sectionPhoto} className="w-[80%] mx-auto  lg:w-full " />
+                    <div className="relative z-[1] ">
+                        <img src={section.sectionPhoto} className="w-[90%] mx-auto  lg:w-full " />
                         <div className="w-[72%] absolute top-[21%] right-[14.5%]  z-[-1] ">
                             <video autoPlay muted loop >
                                 <source src={section.sectionVideo} type='video/mp4' />
