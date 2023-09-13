03cdf8997cf269f18066bf3d27df871aa0c621cd
bacho1927 <bacho.karashvili@gmail.com>
2023-09-13 19:31:42 +0400

    ,

--- src/components/FAQ-page/Main.jsx
+++ src/components/FAQ-page/Main.jsx
@@ -5,7 +5,7 @@ import { MdOutlineArticle } from 'react-icons/md'
 function Main() {
     return (
         <div className=" min-w-[500px]" >
-            <div className="py-12 max-w-[70%]  mx-auto min-w-[500px] px-2  ">
+            <div className="py-12 max-w-[70%]  mx-auto min-w-[500px] px-4  ">
                 < div  >
                     <Link to='/' className="flex items-center text-[#e50914] font-semibold">
                         <AiOutlineArrowLeft />
@@ -24,29 +24,29 @@ function Main() {
                             <p className="mt-4">If you're already a member and would like to learn more about using Netflix, visit Getting started with Netflix.</p>
                         </div>
                     </div>
-                    <aside className="border-t-8 border-red-600 rounded mt-6 xl:ml-16" >
+                    <aside className="border-t-8 border-red-600 rounded mt-6 xl:ml-14" >
 
                         <div className="border border-[#B2B2B2] p-2 rounded ">
                             <h1 className="text-xl font-semibold">Related Articles</h1>
-                            <ul className="space-y-2 underline font-semibold py-4 flex flex-col gap-4">
+                            <ul className="space-y-2 underline font-semibold py-4 flex flex-col gap-2">
 
-                                <li className="flex items-center  ">
+                                <li className="flex items-center gap-2 ">
                                     <MdOutlineArticle className="text-xl" />
                                     <a href="/">Getting started with Netflix</a>
                                 </li>
-                                <li className="flex items-center ">
+                                <li className="flex items-center gap-2 ">
                                     <MdOutlineArticle className="text-xl" />
                                     <a href="/">Billing and Payments</a>
                                 </li>
-                                <li className="flex items-center ">
+                                <li className="flex items-center gap-2">
                                     <MdOutlineArticle className="text-xl" />
                                     <a href="/" >Netflix Gift Cards</a>
                                 </li>
-                                <li className="flex items-center ">
+                                <li className="flex items-center gap-2">
                                     <MdOutlineArticle className="text-xl" />
                                     <a href="/">Can't sign in to Netflix</a>
                                 </li>
-                                <li className="flex items-center ">
+                                <li className="flex items-center gap-2">
                                     <MdOutlineArticle className="text-xl" />
                                     <a href="/">How to create, change, delete profiles </a>
                                 </li>
--- src/components/FAQ-page/Navbar.jsx
+++ src/components/FAQ-page/Navbar.jsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom"
 function Navbar() {
     return (
         <div className="bg-black">
-            <nav className="bg-black flex justify-between mx-auto items-center max-w-[70%]  min-w-[500px]">
+            <nav className="bg-black flex justify-between mx-auto items-center p-2 max-w-[70%]  min-w-[500px]">
                 <div className="flex items-center relative ">
                     <a href="/" className="">
                         <img src={logo} alt="netflix-logo" className='w-28 ' />
 
                     </a>
                     <div className="bg-gray-300 absolute h-6 w-[1px] left-[122px] top-5 "></div>
-                    <Link to="/" className="text-white  font-semibold mx-8 text-md">Help Center</Link>
+                    <Link to="/" className="text-white  font-semibold mx-8 text-sm">Help Center</Link>
                 </div>
-                <div className="text-white flex relative px-4">
-                    <button className="border border-[#696969] font-semibold p-1 px-4 mx-4 rounded-md text-[13px] md:text-md" >Join Netflix</button>
-                    <button className='bg-[#E50914] hover:bg-[#a7131a] transition duration-500 text-white font-semibold p-1 px-4 rounded-md text-[13px]   md:text-md '>Sign In</button>
+                <div className="text-white flex relative px-4 text-[12px] md:text-[15px]">
+                    <button className="border border-[#696969] font-semibold p-1 px-4 mx-4 rounded-md" >Join Netflix</button>
+                    <button className='bg-[#E50914] hover:bg-[#a7131a] transition duration-500 text-white font-semibold p-1 px-4 rounded-md '>Sign In</button>
                 </div>
             </nav>
         </div>
